f5c9545990296645b51502b250b3e7687eef4594
billy3321 <billy3321@gmail.com>
2023-03-24 09:59:14 +0800

    fix the prompt

--- make_book.py
+++ make_book.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import argparse
 import pickle
 import time
@@ -48,7 +47,7 @@ class GPT3(Base):
 
     def translate(self, text):
         print(text)
-        self.data["prompt"] = f"Please help me to translate the following text to {LANG}: \n\n{text}"
+        self.data["prompt"] = f"I want you to act as an {self.language} translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in {self.language}. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level {self.language} words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations, the text is: \n\n{text}"
         r = self.session.post(self.api_url, headers=self.headers, json=self.data)
         if not r.ok:
             return text
@@ -80,7 +79,7 @@ class ChatGPT(Base):
                     {
                         "role": "user",
                         # english prompt here to save tokens
-                        "content": f"Please help me to translate the following text to {LANG}. Please return only translated content not include the origin text. Here is the text: \n\n{text}",
+                        "content": f"I want you to act as an {self.language} translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in {self.language}. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level {self.language} words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations, the text is: \n\n{text}",
                     }
                 ],
             )
@@ -103,7 +102,7 @@ class ChatGPT(Base):
                 messages=[
                     {
                         "role": "user",
-                        "content": f"Please help me to translate the following text to {LANG}. Please return only translated content not include the origin text. Here is the text: \n\n{text}",
+                        "content": f"I want you to act as an {self.language} translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in {self.language}. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level {self.language} words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations, the text is: \n\n{text}",
                     }
                 ],
             )
